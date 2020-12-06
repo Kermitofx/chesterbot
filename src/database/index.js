9a0b8f62a273a9489590fefeb95f93fb20f296ec
Kermitofx <39018448+Kermitofx@users.noreply.github.com>
2020-12-06 17:31:41 -0300

    Update index.js

--- src/database/index.js
+++ src/database/index.js
@@ -2,6 +2,6 @@
 
 const mongoose = require('mongoose')
 
-mongoose.connect(mongo "mongodb+srv://cluster0.sbjds.mongodb.net/91597726" --username 91597726, {useNewUrlParser: true, useUnifiedTopology: true}) //substitua "process.env.DB_STRING" pela string de conexão de seu MongoDb
+mongoose.connect(mongodb+srv://91597726:91597726@cluster0.sbjds.mongodb.net/test, {useNewUrlParser: true, useUnifiedTopology: true}) //substitua "process.env.DB_STRING" pela string de conexão de seu MongoDb
 
 exports.message = mongoose.model('Reply', require('./models/message'))
